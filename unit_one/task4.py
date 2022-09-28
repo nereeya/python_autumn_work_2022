8f4ee386098857aa71191c13f15ecd652e803730
nereeya <113705611+nereeya@users.noreply.github.com>
2022-09-28 15:10:53 +0300

    ОБНОВЛЕНО ( ДЗ от 16.09)

--- unit_one/task4.py
+++ unit_one/task4.py
@@ -4,5 +4,7 @@
 # Мой стек: python, javascript, php
 
 #print("Мой стек:", programming_lang_1, programming lang_2, programming_lang_3)
-
-print("Мой стек: python, javascript, php")
+programming_lang_1 = "python"
+programming_lang_2 = "javascript"
+programming_lang_3 = "php"
+print("Мой стек:", programming_lang_1, ",", programming_lang_2, ",", programming_lang_3)
