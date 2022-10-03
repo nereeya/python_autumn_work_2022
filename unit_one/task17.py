83dd58819f21b0c7ea6919daaf9c25d56d91e212
Екатерина Цибулько <you@example.com>
2022-10-03 15:05:05 +0300

    ДЗ от 26.09

--- unit_one/task17.py
+++ unit_one/task17.py
@@ -4,3 +4,12 @@
 prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
 compute_bill = sum(prices.values())
 print(compute_bill)
+
+#Пыталась создать функцию, но в данном случае ответ не выходит.
+def compute_bill(x):
+    result = sum(x.values())
+
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+x = prices
+answer = compute_bill
+print(answer)
