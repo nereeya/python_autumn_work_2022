e3d352106e889efa2c4d21bd343728e88fc2cb46
Екатерина Цибулько <you@example.com>
2022-10-03 13:23:55 +0300

    ДЗ от 28.09

---
+++ unit_one/task20.py
@@ -0,0 +1,27 @@
+#todo: Выведите все строки данного файла в обратном порядке.
+# Для этого считайте список всех строк при помощи метода readlines().
+
+#Содержимое файла import_this.txt
+#Beautiful is better than ugly.
+#Explicit is better than implicit.
+#Simple is better than complex.
+#Complex is better than complicated.
+
+#выходные данные print(f.readlines(4), "\n",  f.readlines(3), "\n", f.readlines(2), "\n", f.readlines(1))
+#Complex is better than complicated.
+#Simple is better than complex.
+#Explicit is better than implicit.
+#Beautiful is better than ugly.
+
+#В первом случае не получилось вывести каждый
+f = open("import_this.txt", "r")
+p = f.readlines()
+p.reverse()
+print(p)
+
+f = open("import_this.txt", "r")
+i = f.readlines(1)
+n = f.readlines(2)
+j = f.readlines(3)
+k = f.readlines(4)
+print(k, "\n", j, "\n", n, "\n", i)
