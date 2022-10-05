685e391fda56f54dfa5867827339185e90c42967
Екатерина Цибулько <you@example.com>
2022-10-05 16:20:47 +0300

    ДЗ от 26.09

--- unit_one/task17.py
+++ unit_one/task17.py
@@ -1,14 +1,12 @@
-#todo: Создайте функцию compute_bill, считающаю итоговую сумму товаров в чеке.
-#Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
-#Цены хранятся в словаре:
-prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
-compute_bill = sum(prices.values())
-print(compute_bill)
+# todo: Создайте функцию compute_bill, считающаю итоговую сумму товаров в чеке.
+# Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
+# Цены хранятся в словаре:
 
-#Пыталась создать функцию, но
 def compute_bill(x):
-    result = sum(x.values())
-
-compute_bill(prices)
+    return sum(x.values())
 
 
+
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+y = compute_bill(prices)
+print(y)
